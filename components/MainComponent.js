3a2924ccefcd2e3497a1d21f2346087246571a51
Nailtechie817 <britmt817@gmail.com>
2022-05-27 15:30:45 -0400

    editing main and about components

---
+++ components/MainComponent.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import BookNow from './BookNowComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
+import Home from './HomeComponent';
+import Loading from './LoadingComponent';
